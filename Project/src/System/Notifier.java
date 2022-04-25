a021fb20af4406919f69b728d73b72d8d94acae3
talgoldv <63108058+talgoldv@users.noreply.github.com>
2022-04-25 17:27:46 +0300

    Created System Components, needs reference to not existing shop module

---
+++ Project/src/System/Notifier.java
@@ -0,0 +1,7 @@
+package System;
+
+import java.util.ArrayList;
+
+public class Notifier {
+    private ArrayList<Notification> notifications;
+}
---
+++ Project/src/System/System.java
@@ -0,0 +1,9 @@
+package System;
+
+import java.util.ArrayList;
+
+public class System {
+    private Notifier notifier;
+    private ExternalServicesSystem externSystem;
+    //private ArrayList<Shop> shops;
+}
