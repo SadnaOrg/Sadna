49dc13c54c7d0d2198013bee31250e9ed0584f97
guykishon <63450628+guykishon@users.noreply.github.com>
2022-04-25 23:42:37 +0300

    finish product interface
    by maor

--- Project/src/Shops/Product.java
+++ Project/src/Shops/Product.java
@@ -1,10 +1,9 @@
 package Shops;
 
-public class Product {
-
-    private int id;
-    private String name;
-    private double price;
-    private int quantity;
+interface  Product {
+    int getID();
+    String getName();
+    double getPrice();
+    int getQuantity();
 
 }
