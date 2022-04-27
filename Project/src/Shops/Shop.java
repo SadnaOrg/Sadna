3e4bcf55e4cd0817e93cdc1965c549d91b6e3a3f
Yuvalg1 <yuvalgoldvarg@gmail.com>
2022-04-27 19:27:33 +0300

    Implemented product management in Shop

--- Project/src/Shops/Shop.java
+++ Project/src/Shops/Shop.java
@@ -1,6 +1,8 @@
 package Shops;
 
 
+import java.util.HashMap;
+
 public class Shop {
     public enum State {
         OPEN,
@@ -9,5 +11,18 @@ public class Shop {
     private int id;
     private String name;
     private State state;
+    private HashMap<Integer, Product> products;
 
+    public void addProduct(Product p) {
+        products.put(p.getID(), p);
+    }
+
+    public void changeProduct(Product old_product, Product new_product) {
+        products.remove(old_product.getID());
+        products.put(new_product.getID(), new_product);
+    }
+
+    public void removeProduct(Product p) {
+        products.remove(p.getID());
+    }
 }
