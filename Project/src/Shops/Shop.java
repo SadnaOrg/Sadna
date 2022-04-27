ec3eaf02f53978482c1c5c1946bdd31c8798fe5c
Yuvalg1 <yuvalgoldvarg@gmail.com>
2022-04-27 20:09:51 +0300

    Created necessary methods and fields in shop for appointing owner

--- Project/src/Shops/Shop.java
+++ Project/src/Shops/Shop.java
@@ -1,13 +1,10 @@
 package Shops;
 
 
-public class Shop {
-    public enum State {
-        OPEN,
-        CLOSED
-    }
-    private int id;
-    private String name;
-    private State state;
+import Users.SubscribedUser;
+
+public interface Shop {
+    SubscribedUser getFounder();
+    void addShopOwner(SubscribedUser su);
 
 }
---
+++ Project/src/Shops/ShopImpl.java
@@ -0,0 +1,28 @@
+package Shops;
+
+import Users.SubscribedUser;
+
+import java.util.ArrayList;
+
+public class ShopImpl implements Shop {
+
+
+    public enum State {
+        OPEN,
+        CLOSED
+    }
+    private int id;
+    private String name;
+    private State state;
+    private ArrayList<SubscribedUser> owners;
+    private SubscribedUser founder;
+
+
+    public SubscribedUser getFounder() {
+        return founder;
+    }
+
+    public void addShopOwner(SubscribedUser su) {
+        owners.add(su);
+    }
+}
