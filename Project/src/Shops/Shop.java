8b2a9ffa01c9c1742247ea6b7125acf1cb3bf1f3
Yuvalg1 <yuvalgoldvarg@gmail.com>
2022-04-27 19:30:25 +0300

    Created Shop interface for User to follow and implemented parts of it

--- Project/src/Shops/Shop.java
+++ Project/src/Shops/Shop.java
@@ -1,28 +1,8 @@
 package Shops;
 
+public interface Shop {
 
-import java.util.HashMap;
-
-public class Shop {
-    public enum State {
-        OPEN,
-        CLOSED
-    }
-    private int id;
-    private String name;
-    private State state;
-    private HashMap<Integer, Product> products;
-
-    public void addProduct(Product p) {
-        products.put(p.getID(), p);
-    }
-
-    public void changeProduct(Product old_product, Product new_product) {
-        products.remove(old_product.getID());
-        products.put(new_product.getID(), new_product);
-    }
-
-    public void removeProduct(Product p) {
-        products.remove(p.getID());
-    }
+    void addProduct(Product p);
+    void changeProduct(Product old_product, Product new_product);
+    void removeProduct(Product p);
 }
---
+++ Project/src/Shops/ShopImpl.java
@@ -0,0 +1,28 @@
+package Shops;
+
+
+import java.util.HashMap;
+
+public class ShopImpl {
+    public enum State {
+        OPEN,
+        CLOSED
+    }
+    private int id;
+    private String name;
+    private State state;
+    private HashMap<Integer, Product> products;
+
+    public void addProduct(Product p) {
+        products.put(p.getID(), p);
+    }
+
+    public void changeProduct(Product old_product, Product new_product) {
+        products.remove(old_product.getID());
+        products.put(new_product.getID(), new_product);
+    }
+
+    public void removeProduct(Product p) {
+        products.remove(p.getID());
+    }
+}
