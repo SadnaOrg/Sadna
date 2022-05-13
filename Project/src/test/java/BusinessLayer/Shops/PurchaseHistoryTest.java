d6ae7862fc2192882719a370e64731d9449034b1
guykishon <63450628+guykishon@users.noreply.github.com>
2022-05-13 15:47:07 +0300

    adding integration testing for purchase history

--- Project/src/test/java/BusinessLayer/Shops/PurchaseHistoryTest.java
+++ Project/src/test/java/BusinessLayer/Shops/PurchaseHistoryTest.java
@@ -2,6 +2,8 @@ package BusinessLayer.Shops;
 
 import BusinessLayer.Products.Product;
 import BusinessLayer.Users.Basket;
+import BusinessLayer.Users.Guest;
+import BusinessLayer.Users.SubscribedUser;
 import BusinessLayer.Users.User;
 import org.junit.Assert;
 import org.junit.Before;
@@ -19,29 +21,31 @@ import static org.mockito.Mockito.when;
 
 public class PurchaseHistoryTest {
     private PurchaseHistory purchaseHistory;
+    private final SubscribedUser founder = new SubscribedUser("Founder Guy","Guy123456");
 
-    @Mock
     private Shop s1;
-    @Mock
     private Product p1;
-    @Mock
     private Product p2;
-    @Mock
     private User u1;
-    @Mock
     private Basket basket;
-
-    @Rule public MockitoRule rule= MockitoJUnit.rule();
 
     @Before
     public void setUp() {
-
+        s1 = new Shop(100, "shop", founder);
+        p1 = new Product(1, "a", 5, 100);
+        p2 = new Product(2, "c", 15, 500);
     }
     @Test
     public void makePurchaseTest() {
-        when(u1.getName()).thenReturn("Guy");
-        when(s1.getUsersBaskets().get("Guy")).thenReturn(basket);
+        s1.addProduct(p1);
+        s1.addProduct(p2);
+        User u1 = new Guest("Yuval");
+        Basket b = new Basket(s1.getId());
+        b.saveProducts(p1.getID(),10);
+        b.saveProducts(p2.getID(),50);
+        s1.addBasket(u1.getName(), b);
         purchaseHistory = new PurchaseHistory(s1,u1.getName());
+        Assert.assertEquals(0, purchaseHistory.getPast_purchases().size());
         purchaseHistory.makePurchase();
         Assert.assertEquals(1, purchaseHistory.getPast_purchases().size());
 
