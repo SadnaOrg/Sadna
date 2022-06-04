511cb2b847e8f99ff649f15dcf1c5ed678236891
michael zaslavski <michael.za6@gmail.com>
2022-06-04 15:46:44 +0300

    working on admins ORM

--- SadnaORM/src/main/java/com/SadnaORM/Users/ShopAdministrator.java
+++ SadnaORM/src/main/java/com/SadnaORM/Users/ShopAdministrator.java
@@ -1,35 +1,32 @@
 package com.SadnaORM.Users;
 
-import com.SadnaORM.Shops.Shop;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @IdClass(ShopAdministrator.ShopAdministratorPK.class)
-@Table(name = "Admins")
-public abstract class ShopAdministrator {
+public abstract class ShopAdministrator implements Serializable{
     @ElementCollection
     @CollectionTable(
             name = "Administrator Permissions",
             joinColumns = @JoinColumn(name = "username")
     )
     private List<Action> action;
-    @Id
-    @ManyToOne
-    private Shop shop;
+//    @Id
+//    @ManyToOne
+//    private Shop shop;
     @Id
     @ManyToOne
     private SubscribedUser user;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "appointer")
-    private List<ShopAdministrator> appoints;
-    @ManyToOne
-    @JoinColumn(name = "username")
+//    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+//    private List<ShopAdministrator> appoints;
     private String appointer;
 
     public class ShopAdministratorPK implements Serializable {
-        private Shop shop;
+        // private Shop shop;
         private SubscribedUser user;
 
     }
--- SadnaORM/src/main/java/com/SadnaORM/Users/ShopOwner.java
+++ SadnaORM/src/main/java/com/SadnaORM/Users/ShopOwner.java
@@ -6,4 +6,5 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "ShopOwner")
 public class ShopOwner extends ShopAdministrator{
+    boolean isFounder;
 }
