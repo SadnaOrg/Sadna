a8316e8780172be603123a613dd5a72b46dd6fc3
guykishon <63450628+guykishon@users.noreply.github.com>
2022-06-12 22:25:22 +0300

    commit

--- Project/src/main/java/ServiceLayer/Objects/ProductInfo.java
+++ Project/src/main/java/ServiceLayer/Objects/ProductInfo.java
@@ -1,4 +1,4 @@
 package ServiceLayer.Objects;
 
-public record ProductInfo(int shopId, int Id, int quantity, double price, String category) {
+public record ProductInfo(int shopId, int Id, int quantity, double price) {
 }
