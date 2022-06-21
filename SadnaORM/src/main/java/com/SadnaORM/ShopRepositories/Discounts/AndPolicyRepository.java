e416eac32a01c05d2021e8ee813a78bf6ca53bc0
michael zaslavski <michael.za6@gmail.com>
2022-06-21 18:09:49 +0300

    deleted redundant files

--- SadnaORM/src/main/java/com/SadnaORM/ShopRepositories/Discounts/AndPolicyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.SadnaORM.ShopRepositories.Discounts;
-
-import com.SadnaORM.Shops.Discounts.AndPolicy;
-import com.SadnaORM.Shops.Discounts.DiscountPolicy;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AndPolicyRepository extends CrudRepository<AndPolicy, DiscountPolicy.DiscountPolicyPK> {
-}
