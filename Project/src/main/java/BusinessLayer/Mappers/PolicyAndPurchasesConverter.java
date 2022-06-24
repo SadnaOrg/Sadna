ea8d989838f081e44bb51bade551c3ff89b125c5
michael zaslavski <michael.za6@gmail.com>
2022-06-24 19:16:37 +0300

    mapped polices and discounts

--- Project/src/main/java/BusinessLayer/Mappers/PolicyAndPurchasesConverter.java
+++ Project/src/main/java/BusinessLayer/Mappers/PolicyAndPurchasesConverter.java
@@ -1,10 +1,18 @@
 package BusinessLayer.Mappers;
 
+import BusinessLayer.Shops.Polices.Discount.DiscountPlusPolicy;
+import BusinessLayer.Shops.Polices.Discount.DiscountPred;
+import BusinessLayer.Shops.Polices.Discount.ProductByQuantityDiscount;
+import BusinessLayer.Shops.Polices.Discount.ProductDiscount;
+import BusinessLayer.Shops.Polices.Discount.ProductQuantityInPriceDiscount;
+import BusinessLayer.Shops.Polices.Discount.RelatedGroupDiscount;
+import BusinessLayer.Shops.Polices.Discount.ShopDiscount;
+import BusinessLayer.Shops.Polices.Discount.ValidateBasketValueDiscount;
+import BusinessLayer.Shops.Polices.Discount.ValidateProductQuantityDiscount;
 import BusinessLayer.Shops.Polices.Discount.*;
 import BusinessLayer.Shops.Polices.Purchase.*;
 import ORM.Shops.Discounts.DiscountPolicy;
-import ORM.Shops.Discounts.DiscountPred;
-import ORM.Shops.Discounts.ValidateBasketQuantityPred;
+import ORM.Shops.Discounts.*;
 import ORM.Shops.Product;
 
 import java.time.LocalDate;
@@ -16,7 +24,7 @@ public class PolicyAndPurchasesConverter implements Converter{
     @Override
     public DiscountPolicy toEntity(DiscountAndPolicy p, ORM.Shops.Shop shop) {
         int ID = p.getID();
-        Collection<DiscountPred> discountPreds = p.getDiscountPreds().stream().map(pred -> pred.toEntity(this,shop)).toList();
+        Collection<ORM.Shops.Discounts.DiscountPred> discountPreds = p.getDiscountPreds().stream().map(pred -> pred.toEntity(this,shop)).toList();
         DiscountPolicy discountPolicy = p.getDiscountPolicy().toEntity(this, shop);
         return new ORM.Shops.Discounts.AndPolicy(shop, ID, discountPreds, discountPolicy);
     }
@@ -32,7 +40,7 @@ public class PolicyAndPurchasesConverter implements Converter{
     public DiscountPolicy toEntity(DiscountOrPolicy p, ORM.Shops.Shop shop) {
         int ID = p.getID();
         DiscountPolicy discountPolicy = p.getDiscountPolicy().toEntity(this,shop);
-        Collection<DiscountPred> discountPreds = p.getDiscountPreds().stream().map(pred -> pred.toEntity(this,shop)).toList();
+        Collection<ORM.Shops.Discounts.DiscountPred> discountPreds = p.getDiscountPreds().stream().map(pred -> pred.toEntity(this,shop)).toList();
         return new ORM.Shops.Discounts.OrDiscount(shop, ID, discountPolicy, discountPreds);
     }
 
@@ -91,7 +99,7 @@ public class PolicyAndPurchasesConverter implements Converter{
     }
 
     @Override
-    public DiscountPred toEntity(ValidateBasketQuantityDiscount p, ORM.Shops.Shop shop) {
+    public ORM.Shops.Discounts.DiscountPred toEntity(ValidateBasketQuantityDiscount p, ORM.Shops.Shop shop) {
         int ID = p.getID();
         int basketQuantity = p.getBasketquantity();
         boolean cantBeMore = p.isCantBeMore();
@@ -99,7 +107,7 @@ public class PolicyAndPurchasesConverter implements Converter{
     }
 
     @Override
-    public DiscountPred toEntity(ValidateBasketValueDiscount p, ORM.Shops.Shop shop) {
+    public ORM.Shops.Discounts.DiscountPred toEntity(ValidateBasketValueDiscount p, ORM.Shops.Shop shop) {
         int ID = p.getID();
         double basketvalue = p.getBasketvalue();
         boolean cantBeMore = p.isCantBeMore();
@@ -107,7 +115,7 @@ public class PolicyAndPurchasesConverter implements Converter{
     }
 
     @Override
-    public DiscountPred toEntity(ValidateProductQuantityDiscount p, ORM.Shops.Shop shop) {
+    public ORM.Shops.Discounts.DiscountPred toEntity(ValidateProductQuantityDiscount p, ORM.Shops.Shop shop) {
         int ID = p.getID();
         Product product = shop.getProducts().stream().filter(product1 -> p.getProductId()==ID).toList().get(0);
         int productQuantity = p.getProductQuantity();
@@ -216,11 +224,91 @@ public class PolicyAndPurchasesConverter implements Converter{
         throw new IllegalArgumentException("unknown policy type");
     }
 
-    public BusinessLayer.Shops.Polices.Discount.DiscountPred fromDiscountPredicateEntity(ORM.Shops.Discounts.DiscountPred policyEntity){
-        throw new IllegalArgumentException("unknown discount pred type");
+    public BusinessLayer.Shops.Polices.Discount.DiscountPred fromDiscountPredicateEntity(ORM.Shops.Discounts.DiscountPred predicateEntity){
+        if(predicateEntity instanceof ORM.Shops.Discounts.ValidateBasketQuantityPred){
+            int ruleId = predicateEntity.getID();
+            int basketquantity = ((ValidateBasketQuantityPred) predicateEntity).getBasketquantity();
+            boolean cantbemore = ((ValidateBasketQuantityPred) predicateEntity).isCantBeMore();
+            return new ValidateBasketQuantityDiscount(ruleId,basketquantity,cantbemore);
+        }
+        if(predicateEntity instanceof ORM.Shops.Discounts.ValidateBasketValueDiscount){
+            int ruleId = predicateEntity.getID();
+            double basketvalue = ((ORM.Shops.Discounts.ValidateBasketValueDiscount) predicateEntity).getBasketvalue();
+            boolean cantBeMore = ((ORM.Shops.Discounts.ValidateBasketValueDiscount) predicateEntity).isCantBeMore();
+            return new ValidateBasketValueDiscount(ruleId,basketvalue,cantBeMore);
+        }
+        if(predicateEntity instanceof ORM.Shops.Discounts.ValidateProductQuantityDiscount){
+            int ruleId = predicateEntity.getID();
+            int productId = ((ORM.Shops.Discounts.ValidateProductQuantityDiscount) predicateEntity).getProduct().getId();
+            int productQuantity = ((ORM.Shops.Discounts.ValidateProductQuantityDiscount) predicateEntity).getProductQuantity();
+            boolean cantbemore = ((ORM.Shops.Discounts.ValidateProductQuantityDiscount) predicateEntity).isCantbemore();
+            return new ValidateProductQuantityDiscount(ruleId,productId,productQuantity,cantbemore);
+        }
+        throw new IllegalArgumentException("unknown discount predicate type");
     }
 
-    public BusinessLayer.Shops.Polices.Discount.DiscountPolicy fromDiscountPolicyEntity(ORM.Shops.Discounts.DiscountPolicy policy){
+    public BusinessLayer.Shops.Polices.Discount.DiscountRules fromDiscountPolicyEntity(ORM.Shops.Discounts.DiscountPolicy discountEntity){
+        if(discountEntity instanceof ORM.Shops.Discounts.AndPolicy){
+            Collection<BusinessLayer.Shops.Polices.Discount.DiscountPred> discountPreds = ((AndPolicy) discountEntity).getDiscountPreds().stream().map(this::fromDiscountPredicateEntity).toList();
+            DiscountRules discountPolicy = fromDiscountPolicyEntity(((AndPolicy) discountEntity).getDiscountPolicy());
+            return new DiscountAndPolicy(discountPreds,discountPolicy);
+        }
+        if(discountEntity instanceof ORM.Shops.Discounts.DiscountPlusPolicy){
+            int connectId = discountEntity.getID();
+            Collection<DiscountRules> discountPolicies = ((ORM.Shops.Discounts.DiscountPlusPolicy) discountEntity).getDiscountPolicies().stream().map(this::fromDiscountPolicyEntity).toList();
+            return new DiscountPlusPolicy(connectId,discountPolicies);
+        }
+        if(discountEntity instanceof ORM.Shops.Discounts.MaxDiscountPolicy){
+            int connectId = discountEntity.getID();
+            Collection<DiscountRules> discountPolicies = ((MaxDiscountPolicy) discountEntity).getDiscountPolicies().stream().map(this::fromDiscountPolicyEntity).toList();
+            return new DiscountMaxPolicy(connectId,discountPolicies);
+        }
+        if(discountEntity instanceof ORM.Shops.Discounts.OrDiscount){
+            int connectId = discountEntity.getID();
+            DiscountRules discountPolicy = fromDiscountPolicyEntity(((OrDiscount) discountEntity).getDiscountPolicy());
+            Collection<DiscountPred> discountPreds = ((OrDiscount) discountEntity).getDiscountPreds().stream().map(this::fromDiscountPredicateEntity).toList();
+            return new DiscountOrPolicy(connectId,discountPolicy,discountPreds);
+        }
+        if(discountEntity instanceof  ORM.Shops.Discounts.ProductByQuantityDiscount){
+            int discountId = discountEntity.getID();
+            int productId = ((ORM.Shops.Discounts.ProductByQuantityDiscount) discountEntity).getProduct().getId();
+            int productQuantity = ((ORM.Shops.Discounts.ProductByQuantityDiscount) discountEntity).getProductQuantity();
+            double discount = ((ORM.Shops.Discounts.ProductByQuantityDiscount) discountEntity).getDiscount();
+            return new ProductByQuantityDiscount(discountId,productId,productQuantity,discount);
+        }
+        if(discountEntity instanceof ORM.Shops.Discounts.ProductDiscount){
+            int discountId = discountEntity.getID();
+            int productId = ((ORM.Shops.Discounts.ProductDiscount) discountEntity).getProduct().getId();
+            double discount = ((ORM.Shops.Discounts.ProductDiscount) discountEntity).getDiscount();
+            return new ProductDiscount(discountId,productId,discount);
+        }
+        if(discountEntity instanceof  ORM.Shops.Discounts.ProductQuantityInPriceDiscount){
+            int discountId = discountEntity.getID();
+            int prodcutId = ((ORM.Shops.Discounts.ProductQuantityInPriceDiscount) discountEntity).getProduct().getId();
+            int quantity = ((ORM.Shops.Discounts.ProductQuantityInPriceDiscount) discountEntity).getQuantity();
+            double priceForQuanity = ((ORM.Shops.Discounts.ProductQuantityInPriceDiscount) discountEntity).getPriceForQuantity();
+            return new ProductQuantityInPriceDiscount(discountId,prodcutId,quantity,priceForQuanity);
+
+        }
+        if(discountEntity instanceof ORM.Shops.Discounts.RelatedGroupDiscount){
+            int discountId = discountEntity.getID();
+            String category = ((ORM.Shops.Discounts.RelatedGroupDiscount) discountEntity).getCategory();
+            double discount = ((ORM.Shops.Discounts.RelatedGroupDiscount) discountEntity).getDiscount();
+            return new RelatedGroupDiscount(discountId,category,discount);
+        }
+        if(discountEntity instanceof ORM.Shops.Discounts.ShopDiscount){
+            int discountId = discountEntity.getID();
+            int basketQuantity = ((ORM.Shops.Discounts.ShopDiscount) discountEntity).getBasketQuantity();
+            double discount = ((ORM.Shops.Discounts.ShopDiscount) discountEntity).getDiscount();
+            return new ShopDiscount(discountId,basketQuantity,discount);
+        }
+        if(discountEntity instanceof  ORM.Shops.Discounts.XorDiscount){
+            int connectId = discountEntity.getID();
+            DiscountRules discountRules1 = fromDiscountPolicyEntity(((XorDiscount) discountEntity).getDiscountRules1());
+            DiscountRules discountRules2 = fromDiscountPolicyEntity(((XorDiscount) discountEntity).getDiscountRules2());
+            Collection<DiscountPred> tieBreakers = ((XorDiscount) discountEntity).getTieBreakers().stream().map(this::fromDiscountPredicateEntity).toList();
+            return new DiscountXorPolicy(connectId,discountRules1,discountRules2,tieBreakers);
+        }
         throw new IllegalArgumentException("unknown discount policy type");
     }
 }
